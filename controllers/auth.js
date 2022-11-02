7bd5b9afc68c4807472bf119c3cfd612385279d2
Okpe onoja <okpeonoja18@gmail.com>
2022-11-02 01:42:38 +0100

    sorry for too many pushes, im rushing thats why there are too many typos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("./../models/user")
+const user = require("./../models/user")
 const AppError = require("./../utils/appError")
 const catchAsync = require("./../utils/catchAsync")
 const jwt = require("jsonwebtoken")
@@ -13,8 +13,8 @@ const signToken = id =>{
 
 // creating a user
 exports.signUp = catchAsync(async(req, res, next)=>{
-   let token = signToken(User._id)
-    const newuser = await User.create(req.body)
+   let token = signToken(user._id)
+    const newuser = await user.create(req.body)
     res.status(200).json({
         status: "sucess",
         token,
@@ -29,7 +29,7 @@ exports.signUp = catchAsync(async(req, res, next)=>{
 
 exports.login = catchAsync(async(req, res, next)=>{
 
-    const token = signToken(User._id)
+    const token = signToken(user._id)
     const {email, password} = req.body;
 //check if email and password exist 
     if(!email || !password){
@@ -38,9 +38,9 @@ exports.login = catchAsync(async(req, res, next)=>{
 
     //check if passowrd is correct
 
-    const user = user.findOne({email}).select('+password')
+    const User = user.findOne({email}).select('+password')
 
-    if(!user && (!user.comparepassword(password, user.password))){
+    if(!User && (!User.comparepassword(password, User.password))){
         return next(new AppError("i think you need to check your email and passowrd"), 402)
     }
 
@@ -71,7 +71,7 @@ exports.protect = catchAsync(async(req, res, next)=>{
     //verificaton of token
     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET_KEY)
 
-    const currentUser = await User.findById(decoded.id)
+    const currentUser = await user.findById(decoded.id)
 
     //check if user still exist
     if (!currentUser) {
